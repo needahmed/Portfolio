1bae5c146cc77a93b43e5e32956789e2351edadd
needahmed <needahmed2@gmail.com>
2023-10-21 00:41:36 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Ahmed | Personal Portfolio",
+  description: "Ahmed is an emerging full-stack developer.",
 };
 
 export default function RootLayout({
